47f277469b62e25c6abdf9542c9962f86a3888cc
siriusSupreme <guoplc@163.com>
2017-10-28 09:20:15 +0800

    ''

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -15,7 +15,7 @@ try {
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
+ * to our Sirius back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
@@ -34,16 +34,16 @@ let token = document.head.querySelector('meta[name="csrf-token"]');
 if (token) {
     window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
 } else {
-    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+    console.error('CSRF token not found: https://sirius.com/docs/csrf#csrf-x-csrf-token');
 }
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
+ * for events that are broadcast by Sirius. Echo and event broadcasting
  * allows your team to easily build robust real-time web applications.
  */
 
-// import Echo from 'laravel-echo'
+// import Echo from 'sirius-echo'
 
 // window.Pusher = require('pusher-js');
 
